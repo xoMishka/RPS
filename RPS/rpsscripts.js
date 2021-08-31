33de77e6f8f8c4d92a995a571a4b4933bf1dce7d
Amber Jeffers <ajeffers0112@gmail.com>
2021-08-31 18:59:00 -0500

    added game logic

--- RPS/rpsscripts.js
+++ RPS/rpsscripts.js
@@ -27,3 +27,77 @@
 // Scoreboard
     // div#playerSide th
     // div#computerSide th
+
+
+
+
+
+
+
+
+    
+// Computer Randomly Chooses Next
+function computerAI() 
+{
+   srand (time(NULL));
+   let ai = rand() % 3 + 1;
+   /////////// NEED TO DISPLAY CHOICE WITH IMAGE //////////////
+   document.write("Computer chose", ai);
+   return ai;
+}
+
+// Game Logic
+/////////////// NEED TO CONNECT IMAGE AND NUMBERS TOGETHER  ///////////////////
+/////////////// 1 = Rock, 2 = Paper, 3 = Scissors  ///////////////////
+function RockPaperScissors(player, computer) 
+{
+   if (player == 1 && computer == 1)
+   {
+      document.write("It's a tie!")
+   }
+
+   if (player == 1 && computer == 2)
+   {
+      document.write("The computer wins!")
+   }
+
+   if (player == 1 && computer == 3)
+   {
+      document.write("You win!")
+   }
+
+   if (player == 2 && computer == 1)
+   {
+      document.write("You win!")
+   }
+
+   if (player == 2 && computer == 2)
+   {
+      document.write("It's a tie!")
+   }
+
+   if (player == 2 && computer == 3)
+   {
+      document.write("The computer wins!")
+   }
+
+   if (player == 3 && computer == 1)
+   {
+      document.write("The computer wins!")
+   }
+
+   if (player == 3 && computer == 2)
+   {
+      document.write("You win")
+   }
+
+   if (player == 3 && computer == 3)
+   {
+      document.write("It's a tie!")
+   }
+
+   else 
+   {
+      document.write("Please enter 1, 2, or 3")
+   }
+}
