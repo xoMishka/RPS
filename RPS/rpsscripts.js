0ac1ede62589a934f8db86e1f483dab364b01398
Amber Jeffers <ajeffers0112@gmail.com>
2021-08-31 14:04:28 -0500

    added psuedocode to .js

---
+++ RPS/rpsscripts.js
@@ -0,0 +1,16 @@
+// Allow the user to select Rock, Paper, or Scissors
+    // Event Listener
+    // Highlight Selected
+
+// Next the computer will randomly choose 
+    // Highlight Selected
+
+// Generate Message saying who won [maybe already part of game logic the way you wrote it]
+
+// Game Logic
+    // If statements for all variations
+    // Best 2 out of 3 loop 
+
+// Final Win/Loss Message & Final Score
+
+// Scoreboard
